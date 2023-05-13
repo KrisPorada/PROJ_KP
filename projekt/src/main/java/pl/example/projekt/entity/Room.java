8ed0064680bafef4eaa29df5ad9044834eb56f35
KrisPorada <poradox@gmail.com>
2023-05-13 09:56:40 +0200

    ver 5

--- projekt/src/main/java/pl/example/projekt/entity/Room.java
+++ projekt/src/main/java/pl/example/projekt/entity/Room.java
@@ -27,13 +27,6 @@ public class Room {
     @OneToMany(mappedBy = "room")
     private List<Meeting> meetings;
 
-    public void setNumber(String number) {
-    }
-
-    public String getNumber() {
-        return null;
-    }
-
 
 }
 
